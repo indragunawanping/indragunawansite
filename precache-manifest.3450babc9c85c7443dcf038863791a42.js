6b8c09cb5de753b65d3650480eb462480e0988cf
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2020-06-21 19:43:52 +0700

    Updates

---
+++ precache-manifest.3450babc9c85c7443dcf038863791a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5555a97575cdc10e9395fc334fd13512",
+    "url": "/indragunawansite/index.html"
+  },
+  {
+    "revision": "f0a1b03cf58a70d27ae3",
+    "url": "/indragunawansite/static/css/main.f7abd361.chunk.css"
+  },
+  {
+    "revision": "b0e2f755e3c3bea4b217",
+    "url": "/indragunawansite/static/js/2.2fad7123.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/indragunawansite/static/js/2.2fad7123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a1b03cf58a70d27ae3",
+    "url": "/indragunawansite/static/js/main.82e27419.chunk.js"
+  },
+  {
+    "revision": "1115eb20cc71f24fcb79",
+    "url": "/indragunawansite/static/js/runtime-main.05afcfcc.js"
+  },
+  {
+    "revision": "363948657788fae62d190e3a51ed86c3",
+    "url": "/indragunawansite/static/media/logo-ig-v2.36394865.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3293917b267cf4eb4badbd7d59345774.js"
+  "/indragunawansite/precache-manifest.3450babc9c85c7443dcf038863791a42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/indragunawansite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
