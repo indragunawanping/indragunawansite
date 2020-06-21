baa11e3bb70f3964ae93a12f9f8dcfb3c2169ad7
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2020-06-21 18:19:58 +0700

    Updates

---
+++ precache-manifest.f3f770c0b459e90b7297c9dcf1e040a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2561b77fc6f6e7a9c820da13c761954",
+    "url": "/indragunawansite/index.html"
+  },
+  {
+    "revision": "59f088630f46b0f0e473",
+    "url": "/indragunawansite/static/css/main.114641de.chunk.css"
+  },
+  {
+    "revision": "c021f589523f4575f7c1",
+    "url": "/indragunawansite/static/js/2.c27d1c8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/indragunawansite/static/js/2.c27d1c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f088630f46b0f0e473",
+    "url": "/indragunawansite/static/js/main.a8f03fd9.chunk.js"
+  },
+  {
+    "revision": "1115eb20cc71f24fcb79",
+    "url": "/indragunawansite/static/js/runtime-main.05afcfcc.js"
+  },
+  {
+    "revision": "363948657788fae62d190e3a51ed86c3",
+    "url": "/indragunawansite/static/media/logo-ig-v2.36394865.webp"
+  }
+]);
