905be2a165bd584fcfcebe8696a44b56c165314f
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2021-04-11 23:49:42 +0700

    Update project, minor styling.

--- src/master-data/projectCards.ts
+++ src/master-data/projectCards.ts
@@ -2,7 +2,7 @@ const projectCards = [
   {
     image:'https://i.ibb.co/6F5byT4/programming-sewain.png',
     header:'sewa.in',
-    meta:'Februari 2019 - Mei 2019',
+    meta:'February 2019 - May 2019',
     description:'Create Mobile App so the customer can rent a vehicle (like car and motorbike) from someone that also post the rental through the app.',
     extra: 'Lecture Project - Bukan Bertiga',
   },
@@ -16,24 +16,39 @@ const projectCards = [
   {
     image:'https://i.ibb.co/Lps6RDS/programming-re-ceipt.png',
     header:'re-ceipt',
-    meta:'Oktober 2019 - Desember 2019',
+    meta:'October 2019 - December 2019',
     description:'Create Mobile App for spending records (digital receipt) for customer of various types of payments so customer know their expenses on one time and also make income records for the merchant.',
     extra: 'Lecture Project - For Rest',
   },
   {
     image:'https://i.ibb.co/FwwLbXm/programming-cosmed.png',
     header:'COSMED',
-    meta:'Desember 2019 - Februari 2020',
+    meta:'December 2019 - February 2020',
     description:'Create Web App to record patient conditions, symptons, and give recommendations based on SOAP (Subjective - Objective - Assessement - Plan) methodology.',
     extra: 'Agile Technica - COSMED',
   },
   {
     image:'https://i.ibb.co/ngfyJpW/programming-easysr.png',
     header:'Easy SR',
-    meta:'Februari 2020 - Juli 2020',
+    meta:'February 2020 - July 2020',
     description:'Create Web App to make Sustainability Report in digital way for companies as OJK encouragement.',
     extra: 'Agile Technica - Lifecycle Indonesia',
-  }
+  },
+  {
+    image:'https://i.ibb.co/Fgng66Q/programming-skyporter.png',
+    header:'Skyporter',
+    meta:'October 2020 - November 2020',
+    description:'Create Web App as church management for events scheduling, ticketing, attendance, etc.',
+    extra: 'IDstar - SKYBRIDGE',
+  },
+  {
+    image:'https://i.ibb.co/xsmQYws/programming-one-talent.png',
+    header:'One Talent',
+    meta:'September 2020, November 2021 - March 2021',
+    description:'Create Mobile App to give user insight about job opportunity, learning class, bootcamp, event, news, etc.',
+    extra: 'IDstar - ONE',
+  },
+
 ];
 
 export default projectCards;
